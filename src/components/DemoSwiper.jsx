9cfb9cce30e8aea3320f9039d2a32a225792f42e
Mathias Böhm <mathias.boehm@struktuhr.de>
2023-12-18 20:37:04 +0100

    Add Swiper.js

---
+++ src/components/DemoSwiper.jsx
@@ -0,0 +1,32 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/autoplay';
+
+export default function DemoSwiper() {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      autoplay={{
+        delay: 3000
+      }}
+    >
+      <SwiperSlide>
+        <div>
+            <p>Hello</p>
+            <h2>world</h2>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide><img src="images/image_1.jpg" width="400px"></img></SwiperSlide>
+      <SwiperSlide><img src="images/image_2.jpg" width="400px"></img></SwiperSlide>
+      <SwiperSlide><img src="images/image_3.jpg" width="400px"></img></SwiperSlide>
+    </Swiper>
+  );
+};
