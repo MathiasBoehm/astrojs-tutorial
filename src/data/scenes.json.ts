c7acfd7f09f03aff65b07a0d1e5d72e427b3504f
Mathias Böhm <mathias.boehm@struktuhr.de>
2024-03-28 19:25:33 +0100

    add data from ts file

---
+++ src/data/scenes.json.ts
@@ -0,0 +1,15 @@
+import bee from '../assets/images/bee.jpg';
+import orange_sky from '../assets/images/orange_sky.jpg';
+
+export const scenes = [
+    {
+        "name": "Orange Sky",
+        "image" : orange_sky
+    },
+    {
+        "name": "Bee",
+        "image" : bee
+    }
+];
+
+
